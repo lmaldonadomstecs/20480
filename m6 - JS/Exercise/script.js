09f60d8c1356e9aebe3025aa9b49706bd6aa86b2
L.Maldonado <lmaldonado@mstecs.com>
2021-11-01 17:09:46 -0700

    Programming in HTML5, CSS and JS.

---
+++ m6 - JS/Exercise/script.js
@@ -0,0 +1,11 @@
+// Study the following code
+
+function equal() {
+    alert("hello")
+    var x = document.getElementById("screen").value;
+    alert(x);
+}
+
+document.getElementById('equal').addEventListener('click', equal);
+
+
